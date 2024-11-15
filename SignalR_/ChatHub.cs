1d5da9aa698f98e33611dcecff71789d5b14880f
Aya <ayasa3d5@gmail.com>
2024-11-15 11:59:51 +0200

    signalr

--- SignalR_/ChatHub.cs
+++ SignalR_/ChatHub.cs
@@ -14,5 +14,16 @@ namespace SignalR_
         {
             Clients.All.newMessage(name, message);
         }
+
+        public void joinGroup(string groupName, string name)
+        {
+            Groups.Add(Context.ConnectionId, groupName);
+            Clients.OthersInGroup(groupName).newMember(name, groupName);
+        }
+        public void sendToGroup(string gname, string name, string message)
+        {
+            Clients.Group(gname).groupmsg(name, gname, message);
+        }
     }
+   
 }
